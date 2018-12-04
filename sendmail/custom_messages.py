bcd6716bf5a06d903e63bfa58e88f3629efa6b5a
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2018-12-03 22:49:28 -0200

    Adjust in work queue and custom messages

--- sendmail/custom_messages.py
+++ sendmail/custom_messages.py
@@ -1,26 +1,16 @@
-def custom_message01(title_word, body_word, work_description):
+def custom_message(work_description):
     """Custom body and title. Use to send emails, call this on workQueue if necessary.
-    Add new methods like this, case you need to give many custom messages more.
     Use the conditions to determinate a custom message to return, delete this, or comment, case you dont use.
     """
     message = {}
-    if work_description == 1:
-        message['title'] = f'Custom title message - {title_word} example {work_description}.\n'
-        message['body'] = f'Custom body message {body_word} example {work_description}.'
-    elif work_description == 2:
-        message['title'] = f'Custom title message - {title_word} example {work_description}.\n'
-        message['body'] = f'Custom body message {body_word} example {work_description}.'
-    elif work_description == 3:
-        message['title'] = f'Custom title message - {title_word} example. {work_description}\n'
-        message['body'] = f'Custom body message {body_word} example {work_description}.'
+    if work_description == 'work_description01':
+        message['title'] = f'Custom title message - example {work_description}.\n'
+        message['body'] = f'Custom body message - example {work_description}.'
+    elif work_description == 'work_description01':
+        message['title'] = f'Custom title message - example {work_description}.\n'
+        message['body'] = f'Custom body message - example {work_description}.'
+    elif work_description == 'work_description01':
+        message['title'] = f'Custom title message - example {work_description}.\n'
+        message['body'] = f'Custom body message - example {work_description}.'
 
     return message
-
-
-#method_2, for example, without conditions.
-#def custom_message02(title_word, body_word, work_description):
-#    message = {}
-#    message['title'] = f'Custom title message - {title_word} example {work_description}.\n'
-#    message['body'] = f'Custom body message {body_word} example {work_description}.'
-#
-#    return message
--- sendmail/work_queue_execution.py
+++ sendmail/work_queue_execution.py
@@ -25,4 +25,4 @@ elif date_functions.day_of_month() == 21:
 
 #Execute queue work and send e-mail:
 for email in range(0, len(work_queue)):
-    mail_functions.send_mail(work_queue[email], work_queue[email])
+    mail_functions.send_mail(work_queue[email], custom_messages.custom_message(work_descriptions[email]))
